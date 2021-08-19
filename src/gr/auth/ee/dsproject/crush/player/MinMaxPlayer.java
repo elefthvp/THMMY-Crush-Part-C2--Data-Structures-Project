a8e575c5caf1fd436c5c2c99500938f569bc83c5
elefthvp <68590451+elefthvp@users.noreply.github.com>
2021-08-19 17:36:47 +0300

    Update MinMaxPlayer.java

--- src/gr/auth/ee/dsproject/crush/player/MinMaxPlayer.java
+++ src/gr/auth/ee/dsproject/crush/player/MinMaxPlayer.java
@@ -12,8 +12,6 @@ import java.util.ArrayList;
 
 public class MinMaxPlayer implements AbstractPlayer
 {
-  // TODO Fill the class code.
-
   int score;
   int id;
   String name;
@@ -58,26 +56,23 @@ public class MinMaxPlayer implements AbstractPlayer
 
   public int[] getNextMove (ArrayList<int[]> availableMoves, Board board)
   {
-    // TODO Fill the code
-	  Board A;
+	  	Board A;
 		A=CrushUtilities.cloneBoard(board);
 		Node root=new Node(0,A); //etsi i root()?
 		createMySubTree(root,1); //problima edw
 
-        int indexBest = 0;
+		int indexBest = 0;
 
-        indexBest=chooseMove(root);//ki edw
+		indexBest=chooseMove(root);//ki edw
 
-        int[] bestMove = availableMoves.get(indexBest);
+		int[] bestMove = availableMoves.get(indexBest);
 
-        return CrushUtilities.calculateNextMove(bestMove);
-    
+		return CrushUtilities.calculateNextMove(bestMove);
+
   }
 
   private void createMySubTree (Node parent, int depth)
   {
-    // TODO Fill the code
-
 	    ArrayList<int[]> moves=new ArrayList<int[]>();
 	    moves=CrushUtilities.getAvailableMoves(parent.getnodeBoard());
 	    int i;
@@ -95,11 +90,11 @@ public class MinMaxPlayer implements AbstractPlayer
 
   private void createOpponentSubTree (Node parent, int depth)
   {
-    // TODO Fill the code
+    
 
 	    Board A;
 	    A=CrushUtilities.boardAfterFullMove(parent.getparent().getnodeBoard(),parent.getnodeMove());
-	    ArrayList<int[]> moves =new ArrayList<int[]>(); //an vgalei provlima tha kanw copy paste tin apo panw seira
+	    ArrayList<int[]> moves =new ArrayList<int[]>();
 	    moves=CrushUtilities.getAvailableMoves(A);
 	    Board temp;
 	    int i;
@@ -109,44 +104,33 @@ public class MinMaxPlayer implements AbstractPlayer
 	    	child.setnodeEvaluation(-child.evaluate()+parent.getnodeEvaluation());
 	    	parent.setchildren(child);
 	    	
-	    		
-	    	
 	    }
 	    
 
   }
 
   private int chooseMove (Node root)
-  {
-
-    // TODO Fill the code
-	  {  /*για καθε παιδι (επιπεδο 1):εξεταζω τα παιδιά του (εγγόνια), επιλέγω την μικρότερη τιμή και την δίνω σαν τιμή του παιδιού.
-		  έπειτα, απ όλες τις τιμές των παιδιών (επίπεδο 1) επιλέγω τη μεγαλύτερη και επιστρέφω τον δείκτη του, μιας που αυτή είναι η καλύτερη
-		  κίνηση. */
-			  int i,j; //δείκτες επανάληψης
-			  double min;
-			  for(i=0;i<root.getchildren().size();i++){
-				  min=root.getchild(i).getchild(0).getnodeEvaluation();
-				  for(j=1;j<root.getchild(i).getchildren().size();j++){
-					  if(root.getchild(i).getchild(j).getnodeEvaluation()>min){
-						  min=root.getchild(i).getchild(j).getnodeEvaluation();
-					  }
-				  }
-				  root.getchild(i).setnodeEvaluation(min);/*βαζω την αξια καθε μελους του επιπεδου 1 ιση με την μικροτερη απ τα παιδια του
-				  (εγγονια του root) */
+{
+	  int i,j; 
+	  double min;
+	  for(i=0;i<root.getchildren().size();i++){
+		  min=root.getchild(i).getchild(0).getnodeEvaluation();
+		  for(j=1;j<root.getchild(i).getchildren().size();j++){
+			  if(root.getchild(i).getchild(j).getnodeEvaluation()>min){
+				  min=root.getchild(i).getchild(j).getnodeEvaluation();
 			  }
-			  int pos=0;
-			  double max=root.getchild(0).getnodeEvaluation();
-			  for(i=1;i<root.getchildren().size();i++){
-				  if(root.getchild(i).getnodeEvaluation()>max){
-					  max=root.getchild(i).getnodeEvaluation();
-					  pos=i;
-					  }
-			  }
-			  return pos;
-			  
-		    
 		  }
-  }
+		  root.getchild(i).setnodeEvaluation(min);
+	  }
+	  int pos=0;
+	  double max=root.getchild(0).getnodeEvaluation();
+	  for(i=1;i<root.getchildren().size();i++){
+		  if(root.getchild(i).getnodeEvaluation()>max){
+			  max=root.getchild(i).getnodeEvaluation();
+			  pos=i;
+			  }
+	  }
+	  return pos;
 
+ }
 }
